bc00ea3abdf2d058ee364fab51458054d12e4c26
David McGuire <dguigsy@gmail.com>
2019-05-30 22:50:54 +0100

    Create membership.php

---
+++ membership.php
@@ -0,0 +1,31 @@
+<?php
+
+require 'includes/dbh.inc.php';
+include_once('header.php');
+include_once('includes/functions.php');
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Podcross</title>
+</head>
+
+<body>
+
+	
+<div class="benefits">
+	<h4>CONGRATULATIONS ON BECOMING A FULL MEMBER OF PODCROSS!</h4><br>
+
+	<P>We'll send you an email soon with an invite to the Podcross Slack group and details of other cool things...<br><br>
+
+	<button><a href="feed.php">BACK TO THE HOMEPAGE</a></button></P>
+
+	
+
+</div>			
+
+</body>
